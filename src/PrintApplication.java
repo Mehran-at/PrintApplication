2091498b143af535561b606a7da8e8902c70fccc
mehran-at <ajaxcube@gmail.com>
2018-11-14 15:28:49 +0100

    First commit

--- src/PrintApplication.java
+++ src/PrintApplication.java
@@ -1,6 +1,6 @@
 public class PrintApplication {
     public static void main(String[] args) {
-        Article article = new Article("Mehran", "Jan");
+        Article article = new Article("How to win what you want", "Text hereeeee");
         Printer printer = new Printer();
         printer.print(article);
     }
