1404b4509ecc428234e1601fc52d376c933cab0a
Mehran <36104315+Mehran-at@users.noreply.github.com>
2018-11-19 13:23:19 +0100

    Update PrintApplication.java

--- src/PrintApplication.java
+++ src/PrintApplication.java
@@ -1,4 +1,10 @@
-//
+// Hansel arrived home after a hard day of work and he reaches his key to open the door.
+// Develop the code that Hansel uses to create a key and open the door.
+// Follow the instructions below:
+// - You need the following classes: Key, Door and DoorApplication.
+// - The Door has a number. It also has a getNumber method. 
+// - The Key has no attributes. It has the open method that receives a door and says that it’s opening the door with its door number.
+// - In the DoorApplication class main method, create a Door and a Key, and open the door with the key.
 public class PrintApplication {
     public static void main(String[] args) {
         Article article = new Article("How to win what you want", "Text hereeeee");
